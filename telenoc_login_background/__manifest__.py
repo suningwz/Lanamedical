f9aa37ed07eeef612654c488e262b0c0940fef40
jameelahamed21 <88129262+jameelahamed21@users.noreply.github.com>
2021-11-01 15:36:46 +0300

    login background module added

---
+++ telenoc_login_background/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": """Telenoc Login Background""",
+    'summary': 'Telenoc Login Background Image',
+    'version': '14.0.1.0.0',
+    'author': 'Odoo Team, Telenoc',
+    'website': 'http://www.telenoc.org/',
+    'license': 'AGPL-3',
+    "depends": ["base"],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": ["templates.xml", "views/attachment.xml"],
+    "demo": ["demo/demo.xml"],
+    "installable": True,
+    "auto_install": False,
+}
