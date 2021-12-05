277db8a295be0c9e2ab84c167045f021f32392ad
Ahmed Satti <89532559+Ahmedsatti11@users.noreply.github.com>
2021-12-05 16:57:49 +0300

    e-invoicing

---
+++ e-inv15/telenoc_e_invoice/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Telenoc Arabic/English E-Invoice',
+    'version': '1.0.0',
+    'author': 'Odoo Team, Telenoc',
+    'website': 'http://www.telenoc.org/',
+    'category': 'Accounting/Localizations',
+    'description': """
+    Arabic/English for GCC
+""",
+    'license': 'LGPL-3',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+}
